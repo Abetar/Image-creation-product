a49cd6c6d3331ccdc7bd942b505d37d88071e573
Abraham Alexis Gomez Arrazate <47872184+Abetar@users.noreply.github.com>
2025-04-05 16:46:42 -0600

    First comment

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from selenium import webdriver
+import time
+from lxml import html
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ActionChains
+
+print("Hola mundo")
